d1a23c0ce47774da24cfa4af4269b3178588aa07
Bill MacDowell <bill.macdowell@lightelligence.ai>
2020-04-09 13:28:28 -0400

    add libext for unit tests.

--- ut_sim_template.sh
+++ ut_sim_template.sh
@@ -2,11 +2,16 @@
 # This is a template for the bazel rtl_ut rule
 # It is not intended to be run stand-alone
 runmod -t xrun -- \
+    -libext .sv \
+    -libext .svh \
+    -libext .v \
+    -libext .vams \
     -disable_sem2009 \
     -timescale 100fs/100fs \
     -vtimescale 100fs/100fs \
     -define TIMESCALE_STEP_FS=100 \
     -define TIMESCALE_PREC_FS=100 \
+    -enable_single_yvlib \
     {DPI_LIBS} \
     {DEFAULT_SIM_OPTS} \
     {FLISTS} \
