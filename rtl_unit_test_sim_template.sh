d46cde530fdc86fa02718bd06932aed5d3d7b78f
Bill MacDowell <bill.macdowell@lightelligence.ai>
2020-05-05 09:28:56 -0400

    added timescales to rtl_unit_test template

--- rtl_unit_test_sim_template.sh
+++ rtl_unit_test_sim_template.sh
@@ -2,13 +2,17 @@
 # This is a template for the bazel rtl_ut rule
 # It is not intended to be run stand-alone
 runmod -t xrun -- \
+    -define TBV \
     -libext .sv \
     -libext .svh \
     -libext .v \
     -libext .vams \
-    -define TBV \
     -disable_sem2009 \
     -enable_single_yvlib \
+    -timescale 100fs/100fs \
+    -vtimescale 100fs/100fs \
+    -define TIMESCALE_STEP_FS=100 \
+    -define TIMESCALE_PREC_FS=100 \
     {FLISTS} \
     {TOP} \
     $@
