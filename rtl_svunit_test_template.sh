abe412d9df72a79dec85d806867ae1c51db381b5
William Stucker <william.stucker@lightelligence.ai>
2020-09-14 19:19:31 -0400

    Add capability to run svunit tests

---
+++ rtl_svunit_test_template.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+# This is a template for the bazel rtl_ut rule in order to run a SVUnit testbench
+# See http://agilesoc.com/open-source-projects/svunit/
+# It is not intended to be run stand-alone
+# Execute by doing:
+#   bazel run <path>:<target>
+runmod xrun -- \
+    runSVUnit \
+    -s xcelium \
+    --no_abs_path_flist \
+    --no_feedback \
+    {PRE_FLIST_ARGS} \
+    -o . \
+    -c "-define\ TBV" \
+    -c "-libext\ .sv" \
+    -c "-libext\ .svh" \
+    -c "-libext\ .v" \
+    -c "-libext\ .vams" \
+    -c "-disable_sem2009" \
+    -c "-enable_single_yvlib" \
+    -c "-timescale\ 100fs/100fs" \
+    -c "-vtimescale\ 100fs/100fs" \
+    -c "-define\ TIMESCALE_STEP_FS=100" \
+    -c "-define\ TIMESCALE_PREC_FS=100" \
+    {FLISTS} \
+    {POST_FLIST_ARGS} \
+    $@
