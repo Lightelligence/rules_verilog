ada12f59aecef98edbd2bd37fda9810e1043f8ca
Cameron Glass <cameron.glass@lightelligence.ai>
2022-12-01 11:12:06 -0500

    Checkpoint function to run subprocess commands and with robust logging

--- lib/rv_utils.py
+++ lib/rv_utils.py
@@ -138,3 +138,89 @@ def calc_simresults_location(checkout_path):
     regression_directory = checkout_path
     regression_directory = os.path.join(sim_results_home, regression_directory)
     return regression_directory
+
+def run_command(cmd, log, return_stdout=False):
+    """Run arbitrary shell command, print results to stdout and stderr, return (returncode, stdout).
+
+    If return_stdout = True, stdout for the shell command is captured in python and returned from this function
+    instead of appearing in stdout of the console. The stdout from the shell command is always included
+    in the logfile.
+
+    """
+    log.debug("Running shell command: %s", cmd)
+
+    # Big workaround for how the FileHandler works while a subprocess is running to get its stdout/stderr
+    # in both in the terminal's stdout/stderr and the logfile in (approximately) realtime
+    # Without this workaround, the subprocess stdout/stderr wouldn't show up either in the terminal or the log
+    # until after the subprocess finishes. This is bad when running a long program (like flowtool)
+    # because the program's output would be inaccessible until it finished.
+    # Some internet sources recommended using the pexpect module instead of subprocess to solve this problem, but
+    # I've never used that module before and we don't have any preference code for pexpect so I didn't
+    # want to open that can of worms.
+    # You also can't use TemporaryFiles for subprocess stdout/stderr, start the subprocess, enter a polling/sleep loop,
+    # read the TemporaryFiles in the loop, and dump those messages to the log in the loop. I wasn't able to get
+    # that process to work.
+    # Also, adding a | tee to the subprocess command without mucking with the filehandler doesn't work
+    # Since the FileHandle the log is writing to is still active when the subprocess is running, tee can't
+    # actually write to file so it silently fails.
+    # The way we get around this is to temporarily close the stream associated with the FileHandler (but don't
+    # actually remove the FileHandler), running the command piped through tee, then opening a new stream for
+    # the FileHandler. It's very important to note that this only closes the stream associated with the FileHandler,
+    # NOT the FileHandler itself. Closing the FileHandler itself is too big a hammer. If we closed it, we would have to
+    # create a new FileHandler and re-configure it from scratch, which would have bad performance. Instead, we only
+    # close the stream and force in a new StringIO stream. The original FileHandler still exists and operates as
+    # normal, except the messages are sent to this StringIO stream. Since the FileHandler is sending to a StringIO
+    # and the actual file is closed, tee can append to the logfile. After the process finishes and tee has relinquished
+    # control, open a new stream to the original logfile, and connect that stream back to the FileHandler.
+    # It's possible that while the subprocess is running, other messages are sent to the logger. e.g. there was a fork
+    # upstream in the code or there were log messages between the Popen creation and the wait() call. Those log messages
+    # would be correctly sent to the log StreamHandler and to the StringIO. Read all those messages from that StringIO
+    # (which are already formatted correctly) and write them out raw to the re-created file stream in the FileHandler.
+
+    filehandler = log.handlers[1]
+    name = filehandler.stream.name
+    enc = filehandler.stream.encoding
+    filehandler.stream.close()
+
+    # Capture any messages that might happen during the subprocess prevent a:
+    #   ValueError: I/O operation on closed file
+    # from happening on the closed filehandler.stream
+    # The captured messages are replayed to the filehander later when the filehandler.stream is reopened
+    sio = StringIO()
+    filehandler.stream = sio
+
+    # Need to 'set -o pipefail;' before the actual command. Without it, if tee has a returncode of 0, the subprocess
+    # will have a returncode of 0 regardless of if the preceeding command failed.
+    formatted_cmd = "set -o pipefail; {} | tee -a {}".format(cmd, name)
+    popen_kwargs = {'shell': True}
+    stdout_ret_string = ""
+
+    with TemporaryFile() as stdout_fp:
+        if return_stdout:
+            popen_kwargs['stdout'] = stdout_fp
+        pid = subprocess.Popen(formatted_cmd, **popen_kwargs)
+        pid.wait()
+
+        log.debug("Messages during subprocess (approximately, it may be out of order due to threading)")
+        # Save any log messages that happen between disable and reenable of file stream to replay later
+        new_stream = open(name, 'a', encoding=enc)
+        filehandler.stream = new_stream
+
+        # Option
+        sio.seek(0)
+        new_stream.write(sio.read())
+        log.debug("Finished message sync")
+
+        # If return_stdout, read the filehandle
+        if return_stdout:
+            stdout_fp.seek(0)
+            stdout = stdout_fp.read()
+            if stdout:
+                stdout_ret_string = stdout.decode('utf-8')
+
+    returncode = 0
+    if pid.returncode:
+        log.error("Command failed: %s", cmd)
+        returncode = pid.returncode
+
+    return returncode, stdout_ret_string
